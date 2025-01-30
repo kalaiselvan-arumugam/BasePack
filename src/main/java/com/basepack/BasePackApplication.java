45218e3fa1e9f3cf649f23b612a0a6125b8c4a15
Kalaiselvan Arumugam <kalaiselvan.a@teleapps.com>
2025-01-30 20:45:32 +0530

    Initial Commit

---
+++ src/main/java/com/basepack/BasePackApplication.java
@@ -0,0 +1,13 @@
+package com.basepack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasePackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasePackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basepack/BasePackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basepack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasePackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
